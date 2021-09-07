7f359346bf4fc91e5bbc8638b42851d9e06dbb29
zhiheng <zhiheng.hu@klook.com>
2021-09-07 19:58:05 +0800

    feat: add screenshot

--- pytest_seldom/pytest_seldom.py
+++ pytest_seldom/pytest_seldom.py
@@ -91,9 +91,22 @@ def pytest_html_results_table_row(report, cells):
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin("html")
     outcome = yield
     report = outcome.get_result()
     report.description = str(item.function.__doc__)
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        feature_request = item.funcargs['request']
+        driver = feature_request.getfuncargvalue('browser')
+        img = driver.get_screenshot_as_base64()
+        # always add url to report
+        extra.append(pytest_html.extras.url("http://www.example.com/"))
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            extra.append(pytest_html.extras.html("<div>Additional HTML{}</div>".format(str(img))))
+        report.extra = extra
 
 
 def pytest_addoption(parser: Any) -> None:
