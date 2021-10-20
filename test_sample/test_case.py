4600b5b8d3f03c17c684027e686cc82ed9733a6d
defnngj <defnngj@gmail.com>
2021-10-20 23:23:26 +0800

    feat: new poium api

--- test_sample/test_case.py
+++ test_sample/test_case.py
@@ -4,7 +4,7 @@ def test_baidu_search(baidu_page, base_url):
     """
     搜索
     """
-    baidu_page.get(base_url)
+    baidu_page.open(base_url)
     baidu_page.search_input.send_keys("pytest")
     baidu_page.search_button.click()
     baidu_page.sleep(2)
@@ -15,7 +15,7 @@ def test_baidu_search_setting(baidu_page, base_url):
     """
     搜索设置
     """
-    baidu_page.get(base_url)
+    baidu_page.open(base_url)
     baidu_page.settings.click()
     baidu_page.search_setting.click()
     baidu_page.sleep(2)
